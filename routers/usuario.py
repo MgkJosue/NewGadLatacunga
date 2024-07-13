795c8d2c759caaea9b246682465d598bbd596d9e
MgkJosue <josue.alvarez9@hotmail.com>
2024-07-13 15:37:02 -0500

    Error solucionado

--- routers/usuario.py
+++ routers/usuario.py
@@ -9,18 +9,23 @@ router = APIRouter()
 
 @router.get("/usuario_ruta/{usuario_id}", response_model=list[UsuarioRutaResult])
 async def obtener_ruta_usuario(usuario_id: int, current_user: dict = Depends(get_current_user)):
+    query = "SELECT * FROM UsuarioRuta(:usuario_id)"
+    values = {"usuario_id": int(usuario_id)}  # Asegurando que el valor se pase como string
+    
     try:
-        query = text("SELECT * FROM UsuarioRuta(:usuario_id)").bindparams(
-            bindparam("usuario_id", usuario_id)
-        )
-        result = await database.fetch_all(query)
+        # Ejecuta la consulta pasando el parámetro correctamente
+        result = await database.fetch_all(query=query, values=values)
         if not result:
             raise HTTPException(status_code=404, detail="No se encontraron rutas para este usuario")
         return result
     except SQLAlchemyError as e:
         raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Error en la base de datos"
+            status_code=500, detail="Error en la base de datos"
         ) from e
+    except Exception as e:
+        raise HTTPException(
+            status_code=500, detail=str(e)
+        )
     
 @router.get("/obtenerUsuarios/")
 async def obtener_usuarios(current_user: dict = Depends(get_current_user)):
